03d3a85d63fbe7af3e8aeae66b491011d73c08fd
Dyego Castro <dcastrocerqueira33@gmail.com>
2023-05-14 00:28:06 -0300

    Atualizado

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let n1 = Math.floor( Math.random() * 6 ) + 1
-let randomDice1 = 'dice' + n1 + '.png'
-let randomImage1 = 'img/' + randomDice1
+let randomDice1 = 'img/dice' + n1 + '.png'
 let image1 = document.querySelectorAll('img')[0]
-image1.setAttribute('src', randomImage1)
+image1.setAttribute('src', randomDice1)
+
+let n2 = Math.floor( Math.random() * 6 ) + 1
+let randomDice2 = 'img/dice' + n2 + '.png'
+let image2 = document.querySelectorAll('img')[1]
+image2.setAttribute('src', randomDice2)
+
+if(n1 > n2){
+    document.querySelector('h1').innerHTML = 'Jogador 1 Venceu! 🏆'
+}else if(n2 > n1){
+    document.querySelector('h1').innerHTML = 'Jogador 2 Venceu! 🏆'
+}else{
+    document.querySelector('h1').innerHTML = 'Empate'
+}
